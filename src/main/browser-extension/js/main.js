bff17bad8eae9717ce2dc74ff44ef3ae946a5493
かとちん <tkatochin>
2019-06-12 20:59:56 +0900

    ニュースフィード、プロフィール、色々周ってスタイルが崩れないよう調整した。pdf viewerが表示できないので、pdf viewerが無いことを確認してから図やサムネイルをscalableにさせるよう変更。

--- src/main/browser-extension/js/main.js
+++ src/main/browser-extension/js/main.js
@@ -1,12 +1,33 @@
 $(window).on("load", function(){
   setInterval(function(){
-    ["div#contentArea", "#timeline_tab_content>div>div"].forEach(function(q) {
+    let e = document.querySelector("div#content>div");
+    if (e && e.style.position != "absolute") {
+      e.style.position = "absolute";
+      e.style.display = "block";
+      e.style.flexGrow = "unset";
+    }
+
+    ["#workGalahadWebChromeEntity",
+     "#workGalahadWebChromeEntity div#contentArea",
+     "#timeline_tab_content>div>div",
+     "._7ibx"].forEach(function(q) {
       let e = document.querySelector(q);
       if (e && e.style.width != "100%") {
-        e.style.position = "absolute";
+        e.style.maxWidth = "100%";
         e.style.width = "100%";
         e.style.left = "0";
       }
     });
+    document.querySelectorAll("._6uka").forEach(function(e) {
+      e.style.width = "100%";
+      e.style.left = "0";
+    });
+
+    document.querySelectorAll(".mtm:not(.scalable)").forEach(function(e) {
+      if (null == e.querySelector("._50zm")) {
+        e.classList.add("scalable");
+      }
+    });
+
   }, 100);
 });
