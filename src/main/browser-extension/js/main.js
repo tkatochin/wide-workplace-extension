9f66c4bf2d1d76b051318bc0ccf38cf839755e3a
かとちん <tkatochin>
2019-06-12 16:59:21 +0900

    古いデザインのWorkplaceを捨て、新デザインに特化。イメージなども解像度に応じてスケーラブルに表示するようにした。

--- src/main/browser-extension/js/main.js
+++ src/main/browser-extension/js/main.js
@@ -1,54 +1,12 @@
-const updateRightPane = function() {
-  $("#rightCol")
-    .attr("tabindex", "0")
-    .removeClass("pined")
-    .off("focus")
-    .on("focus", function(ev) {
-      $("#rightCol").addClass("pined");
-    })
-    .off("mousedown")
-    .on("mousedown", function(ev) {
-      ev.stopPropagation();
-    });
-  $(document.body)
-    .off("mousedown")
-    .on("mousedown", function(ev) {
-      $("#rightCol").removeClass("pined");
-    });
-}
-
 $(window).on("load", function(){
-  $(document.head).append(
-    $("<link/>").attr("rel", "stylesheet").attr("type", "text/css")
-      .attr("href", chrome.runtime.getURL("css/patch.css"))
-  );
-  // rightColが書き換えられてるのに、MutationObserverもhashchangeも検知しない…。
-  // 仕方がないので div#content.fb_content のこの更新検知で行う。
-  let observer = new MutationObserver(mutations=>{
-    mutations.some(mutation=>{
-      let nodes = Array.from(mutation.addedNodes);
-      nodes.push(mutation.target);
-      nodes = nodes.concat(Array.from(mutation.removedNodes));
-      nodes.some(node=>{
-        if (node instanceof HTMLDivElement && $(node).attr("id")) {
-          if ($(node).attr("id") == "content") {
-            console.dir(node);
-            // 子要素の中のrightColが作り変えられると予測して時間を空けて実施
-            setTimeout(function(){
-              let rightCol = $("#rightCol");
-              if (rightCol[0] && rightCol.attr("tabindex") != "0") {
-                updateRightPane();
-              }
-            }, 300);
-            return true;
-          }
-        }
-      });
+  setInterval(function(){
+    ["div#contentArea", "#timeline_tab_content>div>div"].forEach(function(q) {
+      let e = document.querySelector(q);
+      if (e && e.style.width != "100%") {
+        e.style.position = "absolute";
+        e.style.width = "100%";
+        e.style.left = "0";
+      }
     });
-  });
-  observer.observe(document.querySelector("#content"), {
-    childList: true,
-    subtree: true,
-  });
-  updateRightPane();
+  }, 100);
 });
